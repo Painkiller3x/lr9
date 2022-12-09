c45fcb768054768111d0af892744e669972209e3
Painkiller3x <116508370+Painkiller3x@users.noreply.github.com>
2022-12-09 16:21:27 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var name = prompt("Введіть ваше ім'я");
+if(name==""){name="NoName";}
+const deck ={"img//2-Foglet.png":2,
+              "img//3-MMaker.png":3,
+			  "img//4-Pellar.png":4,
+			  "img//5-Mahakam.png":5,
+			  "img//6-Roderick.png":6,
+			  "img//7-Palmerin.png":7,
+			  "img//8-Hillock.png":8,
+			  "img//9-Vigo.png":9,
+			  "img//10-Dettlaff.png":10,
+			  "img//11-Dagon.png":11
+			  };
+var player=0, comp=0,count = 0;
+btn.addEventListener('click', () => {
+	var a = Math.floor(Math.random() * 10);
+	var b = Math.floor(Math.random() * 10);
+	document.getElementById('c1').setAttribute("src", Object.keys(deck)[a]);
+    document.getElementById('c2').setAttribute("src", Object.keys(deck)[b]);
+    player += Object.values(deck)[a];
+    comp += Object.values(deck)[b];
+	count++;
+	if(count==3)
+	{
+        if(((player<=21)&&(player>comp))||((comp>21)&&(player<=21))){alert("Виграв:"+name)}
+	      if(((comp<=21)&&(comp>player))||((player>21)&&(comp<=21))){alert("Виграв:бот")}
+			if(((comp>21)&&(player>21))||(player==comp)){alert("Нічия")}
+	}
+	if(count>=3){document.getElementById("btn").disabled = true;}
+	score.textContent = name +":"+player+" Comp:"+comp;
+});
+btn2.addEventListener('click', () => {
+	document.getElementById("btn").disabled = false;
+    player = 0;
+	comp = 0;
+	count = 0;
+	score.textContent = name +":"+player+" Comp:"+comp;
+});
